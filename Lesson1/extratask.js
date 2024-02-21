489bdfa94bb771d3e2c7c6b95104127fcdb83a26
TheFantom1 <147336863+TheFantom1@users.noreply.github.com>
2024-02-21 22:05:43 +0200

    extratask.js

--- Lesson1/extratask.js
+++ Lesson1/extratask.js
@@ -1,3 +1,6 @@
+//Додаткове для тих хто цікавився prompt`oм
+//- За допомогою 3х різних prompt() отримати 3 слова які являються вашими Імям, По-Батькові та роками. та вивести в консоль
+
 let firstName = prompt('First name: ' )
 let middleName = prompt('Middle name: ')
 let age = prompt('Age: ')
