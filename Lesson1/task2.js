f56ccfdf220861f4c7b48d45401eb6b371026bec
TheFantom1 <147336863+TheFantom1@users.noreply.github.com>
2024-02-21 22:04:44 +0200

    task2.js

--- Lesson1/task2.js
+++ Lesson1/task2.js
@@ -1,3 +1,5 @@
+//- Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person (Не об'єкт, просто за допомоги конкатенації)
+
 const firstName = 'Arsenii'
 const middleName = 'Lubomirovich'
 const lastName =  'Laiuk'
